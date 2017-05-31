46a445e5641c0a9c7193564d081f962fce621af5
James E. Marca <james@activimetrics.com>
2017-05-30 19:30:05 -0700

    improving test coverage, changing API

--- test/test_pgpool.js
+++ test/test_pgpool.js
@@ -12,102 +12,119 @@ const rootdir = path.normalize(__dirname)
 const config_file = rootdir+'/../test.config.json'
 const config_okay = require('config_okay')
 
-tap.plan(6)
+tap.plan(5)
 
 config_okay(config_file)
-    .then( config => {
+    .then(async ( config ) => {
         //console.log('got config')
         // config has config parameters for pg_pool
-        tap.test('init function exists',function (t) {
-            t.plan(1)
-            t.pass('testing pool_init')
-            t.end()
-        })
-        tap.test('expect a failure',async function(t) {
-            t.plan(2)
-            t.ok(pg_pool.pool_init,'have pool init')
-            return pg_pool.pool_init()
-                .then( r => {
-                    // console.log('should not be in this branch')
-                    t.fail ('should have crashed')
-                    t.end()
-                    return null
-                } )
-                .catch( e => {
-                    // console.log('inside expected catch statement')
-                    t.pass('failed to init without config file')
-                    t.end()
-                    return null
-                })
 
-        })
-
-        tap.test('expect a failure to get pool',async function(t) {
-            t.plan(2)
+        await tap.test('expect a failure to get pool',async function(t) {
+            //t.plan(2)
             t.ok(pg_pool.get_pool,'have get_pool fn')
-            return pg_pool.get_pool({})
-                .then( r => {
-                    t.fail('should have crashed')
-                    t.end()
-                    return null
-                })
-                .catch (e => {
-                    t.pass('failed to get pool without config')
-                    t.end()
-                    return null
-                })
+            const empty_pool = await  pg_pool.get_pool({})
+            // empty config results in broken pool
+            //console.log('mpty pool is ',empty_pool)
+            let client
+            try {
+                client = await empty_pool.connect()
+                t.fail ('should have crashed')
+                if(client) client.release()
+            }
+            catch(e){
+                // console.log('pool.connect failed as expected',e)
+                t.match(e,/does not exist/,'cannot log in with bad values')
+                t.pass('empty (default) connection parameters')
+            }
+            finally{
+                t.end()
+            }
+
         })
 
-        tap.test('expect a failure',async function(t) {
-            t.plan(2)
-            t.ok(pg_pool.pool_init,'have pool init')
-            return pg_pool.pool_init({})
-                .then( r => {
-                    // console.log('should not be in this branch')
-                    t.fail ('should have crashed')
-                    t.end()
+        await tap.test('expect a failure to get pool',async function(t) {
+            //t.plan(2)
+            t.ok(pg_pool.get_pool,'have get_pool fn')
+            try {
+                const empty_pool = await  pg_pool.get_pool()
+                t.fail ('should have crashed')
+                // empty config results in broken pool
+                //console.log('mpty pool is ',empty_pool)
+            }
+            catch(e){
+                // console.log('pool.connect failed as expected',e)
+                t.match(e,/empty config file/,'cannot log in without config')
+                t.pass('empty (default) connection parameters')
+            }
+            finally{
+                t.end()
+            }
+
+        })
+
+
+        await tap.test('expect a failure',async function(t) {
+            //t.plan(2)
+            return pg_pool.get_pool({'postgresql':{'username':'slub',
+                                                   'password':'grobblefruit'}})
+                .then( async (broken_pool) => {
+                    let client
+                    try {
+                        client = await broken_pool.connect()
+                        console.log('pool.connect did not fail as expected',client)
+                        t.fail ('should have crashed')
+                        client.release()
+                    }
+                    catch(e){
+                        console.log('pool.connect failed as expected')
+                        t.match(e,/does not exist/,'user name fail')
+                        t.pass('bad connection parameters')
+                    }
                     return null
-                } )
+                })
                 .catch( e => {
-                    // console.log('inside expected catch statement')
+                    console.log('inside expected catch statement',e)
+
                     t.pass('failed to init without config file')
                     t.end()
+
                     return null
                 })
 
         })
 
-        tap.test('expect pool to init okay',async function(t) {
-            t.plan(2)
-            t.ok(pg_pool.pool_init,'have pool init')
-            return pg_pool.pool_init(config)
-            //await init;
-                .then( (pool)=>{
-                    // console.log('then statement')
-                    t.pass('initialize db pool okay')
-                    t.end()
-                    return null
-                })
-                .catch( (e) => {
-                    // console.log('catch statement')
-                    t.fail('failed to initialize db pool okay')
-                    t.end()
-                    return null
-                })
+        await tap.test('expect success getting pool 1',async function(t) {
+            //t.plan(1)
+            try {
+                const pool = await pg_pool.get_pool(config)
+                const client = await pool.connect()
+                t.pass('get pool okay')
+                await client.release()
+                t.end()
+                return null
+            }
+            catch (e) {
+                // console.log('catch statement')
+                t.fail('failed to initialize db pool okay')
+                t.end()
+                return null
+            }
         })
-        tap.test('expect success getting pool',async function(t) {
-            t.plan(1)
-            return pg_pool.get_pool({})
-                .then( r => {
-                    t.pass('should not crashed')
-                    t.end()
-                    return null
-                })
-                .catch (e => {
-                    t.fail('failed to get pool with valid init')
-                    t.end()
-                    return null
-                })
+
+        await tap.test('expect success getting pool 2',async function(t) {
+            //t.plan(1)
+            try {
+                const pool = await pg_pool.get_pool(config)
+                const client = await pool.connect()
+                t.pass('should not crashed')
+                await client.release()
+                t.end()
+                return null
+            } catch (e){
+                t.fail('failed to get pool with valid init')
+                t.end()
+                return null
+            }
         })
 
         tap.end()
